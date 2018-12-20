266cd45ee11f412b7bb137635c5a63eaf6e5e1e2
kristelle feghali <zcabfeg@ucl.ac.uk>
2018-12-20 19:23:36 +0200

    map 0 to 1

--- loadNpyFile.py
+++ loadNpyFile.py
@@ -1,5 +1,21 @@
 import numpy as np
+import csv
 
-data = np.load('correspondences-1_500000.npy')
+mapping_csv = csv.reader(open('/Users/macbook/Desktop/mapping-darknet.csv', 'rt'), delimiter=',')
+writer = csv.writer(open('/Users/macbook/Desktop/mapping-darknet copy.csv', 'wt'))
+npyarray=[]
+count=0
+data = []
+data = np.load('/Users/macbook/Desktop/minimise_user-1_500000.npy')
 
-print (len(data),data)
+for row in mapping_csv:
+    index = int(row[2])
+    npyarray.append(data[index])
+    count += 1
+    if count % 1000 == 0:
+        print(count, '/1.817.311')
+
+np.save('/Users/macbook/Desktop/output.npy', npyarray)
+
+print('done')
+
--- mapAddtoID.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-csv_file = csv.reader(open('/Users/macbook/Desktop/map_addresses-1_500000.csv', 'rt'), delimiter=',')
-for row in csv_file:
-    print(row)
